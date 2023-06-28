c51780448684263027542ac368b7e435c2a6047a
hukumran-san <71653875+hukumran-san@users.noreply.github.com>
2023-06-28 19:26:52 +0300

    v 0.0.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import * as React from "react";
 import "./App.css";
-import Header from "./Header";
-import Content from "./Content";
-import Footer from "./Footer";
 import Employees from "./Employees";
 
 function App() {
